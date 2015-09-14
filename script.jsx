fd147d2b5504add68dbe596b5852b0b911c8d4cd
jshuadvd <jshuadvd@icloud.com>
2015-09-13 23:54:51 -0700

    Corrected erros and added font style

--- script.jsx
+++ script.jsx
@@ -1,28 +1,26 @@
 var Card = React.createClass({
-
   render: function() {
     return (
+
       <div>
         <img src="" />
         <h3>Name Here</h3>
-        </hr>
-
+        <hr/>
       </div>
     );
   }
-
 });
 
 var Main = React.createClass({
-
   render: function() {
     return (
       <div>
-        <Card />
-      <div />
+      <Card />
+      </div>
+
+
     );
   }
-
 });
 
-React.render( < Main / > , document.getElementById("root"));
+React.render( <Main / >, document.getElementById("root"));
