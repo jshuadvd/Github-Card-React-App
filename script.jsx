fc6bfee1011091b3dc7f0dffae138ee18a16e0bd
jshuadvd <jshuadvd@icloud.com>
2015-09-15 22:33:38 -0700

    Added the functionality so that the users statment is general and it pulls info for which ever user you enter

--- script.jsx
+++ script.jsx
@@ -4,32 +4,38 @@ var Card = React.createClass({
   },
   componentDidMount: function() {
     var component = this;
-    $.get("https://api.github.com/users/jshuadvd", function(data) {
+    $.get("https://api.github.com/users/" + this.props.login, function(data) {
       component.setState(data);
     });
   },
   render: function() {
     return (
 
-      <div>
-        <img src={this.state.avatar_url} width="80" />
-        <h3>{this.state.name}</h3>
-        <hr/>
-      </div>
+      < div >
+      < img src = {
+        this.state.avatar_url
+      }
+      width = "80" / >
+      < h3 > {
+        this.state.name
+      } < /h3> < hr / >
+      < /div>
     );
   }
 });
 
 var Main = React.createClass({
   render: function() {
-    return (
-      <div>
-      <Card login="JSHUADVD" />
-      </div>
+    return ( < div >
+      < Card login = "JSHUADVD" / >
+      < Card login = "ericdouglas" / >
+
+
+      < /div>
 
 
     );
   }
 });
 
-React.render( <Main / >, document.getElementById("root"));
+React.render( < Main / > , document.getElementById("root"));
